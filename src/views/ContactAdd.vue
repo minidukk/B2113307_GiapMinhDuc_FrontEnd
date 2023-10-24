39530cdab8ee6be1a447e94479483f2fac9cd1e0
minidukk <vanducg15@gmail.com>
2023-10-24 11:16:09 +0700

    Tao trang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="page">
+        <h4>Tạo liên hệ mới</h4>
+        <ContactForm 
+            :contact="contact" 
+            @submit:contact="addContact" 
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+  
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+
+export default {
+    components: {
+        ContactForm,
+    },
+
+    data() {
+        return {
+            contact: {},
+            message: "",
+        };
+    },
+
+    methods: {
+        async addContact(data) {
+            try {
+                await ContactService.create(data);
+                this.message = 'Đã thêm liên hệ!';
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+}
+</script>
